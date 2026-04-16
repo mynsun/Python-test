e1f1c1edbf960f7fa58ed3f52d14c1ab7220bbdb
mynsun <alstjs3022@naver.com>
2026-04-16 11:13:14 +0900

    1st Python commit

---
+++ 1_data.py
@@ -0,0 +1,21 @@
+type(10)
+
+print(1/2)
+print(5//3)
+print(5**3)
+print(20%3)
+
+print('Hello' + 'World!')
+print('Hello'*2)
+
+num=4
+print(num)
+num *= 3
+print(num)
+num/= 3
+print(num)
+num**= 2
+print(num)
+
+print('Hello' < 'World!')
+print('홍길동' != '허균')
